33106aabef06788c0c1beb0829e0be6f5b9dcfb9
Deniz <dnz66@hotmail.com>
2023-08-10 19:40:36 -0400

    variables and getters and setters were added to car class, method1 was added tpo Plane class

--- src/Vehicle/Car.java
+++ src/Vehicle/Car.java
@@ -1,9 +1,36 @@
 package Vehicle;
 
 public class Car {
+    String make;
+    String model;
+    int year;
     public static void main(String[] args) {
         for(int i = 0; i <10; i++) {
             System.out.println("Car goes");
         }
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 }
--- src/Vehicle/Plane.java
+++ src/Vehicle/Plane.java
@@ -4,4 +4,7 @@ public class Plane {
     public static void main(String[] args) {
         System.out.println("plane flies");
     }
+    public void method1() {
+        System.out.println("Planes go fast");
+    }
 }
